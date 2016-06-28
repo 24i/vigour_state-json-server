4011168525e6e3fc7f5ac4bee06cce6f997cc797
Jim de Beer <jim@vigour.io>
2016-06-28 12:32:36 +0200

    complete tests

--- test/index.js
+++ test/index.js
@@ -6,28 +6,58 @@ const http = require('http')
 const port = 8888 // use freeport more clean
 
 test('server state', (t) => {
-  const state = s({
-    one: 'hello',
-    two: 'bye!',
-    nest: {
-      a: {
-        b: {
-          c: 'c'
+  var step = 0
+  const state = s()
+
+  const results = [
+    { nest: { a: { b: { c: 'c' } } } },
+    { nest: { a: { b: { c: 'd' } } }, x: true }
+  ]
+
+  const input = [
+    () => state.set({
+      nest: {
+        a: {
+          b: {
+            c: 'c'
+          }
         }
       }
+    }),
+    () => {
+      const c = state.nest.a.b.c
+      c.set('e')
+      setTimeout(() => c.set('f'))
+      setTimeout(() => c.set('d'), 50)
+      setTimeout(() => state.set({ x: true }), 100)
     }
-  })
-  const server = createServer(t, port)
-  http.request({
-    host: 'localhost',
-    port: port,
-    method: 'GET'
-  }, (res) => {
-    const str = ''
-    res.on('data', (chunk) => { str += chunk })
-    res.on('end', () => {
-      console.log(JSON.parse(str))
-      t.end() // more ofc
-    })
-  }).end()
+  ]
+
+  t.plan(results.length)
+
+  const server = createServer(state, port)
+
+  input[step]()
+
+  function doRequest() {
+    http.request({
+      host: 'localhost',
+      port: port,
+      method: 'GET'
+    }, (res) => {
+      var str = ''
+      res.on('data', (chunk) => { str += chunk })
+      res.on('end', () => {
+        t.same(JSON.parse(str), results[step], 'correct respone case:' + step)
+        if (step !== input.length - 1) {
+          step++
+          input[step]()
+          doRequest()
+        } else {
+          server.close()
+        }
+      })
+    }).end()
+  }
+  doRequest()
 })
